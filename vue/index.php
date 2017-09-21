0ae1e50e304ebe79f20a075297d79c44d69c2863
Laure-Anne Urbaniak <laureane.urbaniak@gmail.com>
2017-09-21 09:49:31 +0200

    add form in vue folder

---
+++ vue/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1>INSCRIPTION</h1>
+    <form class="" action="" method="post">
+      <h3>Entrez votre pseudo :</h3>
+      <input type="text" name="pseudo" value="" placeholder="pseudo"><br>
+      <h3>Entrez votre pseudo :</h3>
+      <input type="password" name="password" value="" placeholder="password"><br>
+      <h3>Entrez votre email :</h3>
+      <input type="mail" name="mail" value="" placeholder="email">
+    </form>
+  </body>
+</html>
